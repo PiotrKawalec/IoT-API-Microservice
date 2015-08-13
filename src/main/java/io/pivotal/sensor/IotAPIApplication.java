2df6ee1ef90296ba4021c815228a2f42eb06b9ab
P K <pkawalec@pivotal.io>
2015-08-13 12:07:31 +0100

    final fix

--- src/main/java/io/pivotal/sensor/IotAPIApplication.java
+++ src/main/java/io/pivotal/sensor/IotAPIApplication.java
@@ -1,9 +1,20 @@
 package io.pivotal.sensor;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
 public class IotAPIApplication {
 
     public static void main(String[] args) {
