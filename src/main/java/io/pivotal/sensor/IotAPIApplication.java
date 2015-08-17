3711dafe3340f4aadabe48b187dbbf96c1fc08de
P K <pkawalec@pivotal.io>
2015-08-17 17:12:46 +0100

    configuration refactorized

--- src/main/java/io/pivotal/sensor/IotAPIApplication.java
+++ src/main/java/io/pivotal/sensor/IotAPIApplication.java
@@ -8,13 +8,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
 @SpringBootApplication
 @EnableEurekaClient
-@EnableDiscoveryClient
 public class IotAPIApplication {
 
     public static void main(String[] args) {
